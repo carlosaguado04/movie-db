edbdf14a067146f26143d010d10f831f5a742f7e
Carlos <carlosa@Carloss-MacBook-Pro.local>
2022-01-23 01:50:18 +0300

    first

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/popular?api_key=29fe3343109db62f7473a92c31283a1f&language=en-US&page=1`
+		);
+
+		const data = await res.json();
+		if (res.ok) {
+			return {
+				props: { popular: data.results }
+			};
+		}
+	}
+</script>
+
+<script>
+	import PopularMovies from '../components/PopularMovies.svelte';
+	export let popular;
+</script>
+
+<section><PopularMovies {popular} /></section>
---
+++ src/routes/movie/[id].svelte
@@ -0,0 +1,31 @@
+<script context="module">
+	export async function load({ fetch, params }) {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/${params.id}?api_key=29fe3343109db62f7473a92c31283a1f&language=en-US`
+		);
+
+		const movieDetails = await res.json();
+		if (res.ok) {
+			return {
+				props: { movieDetails }
+			};
+		}
+	}
+</script>
+
+<script>
+	export let movieDetails;
+	console.log(movieDetails);
+</script>
+
+<div class="movie-details">
+	<div class="img-container">
+		<img
+			src={'https://image.tmdb.org/t/p/original' + movieDetails.backdrop_path}
+			alt={movieDetails.title}
+		/>
+	</div>
+	<div class="txt-container">
+		<h1>{movieDetails.title}</h1>
+	</div>
+</div>
